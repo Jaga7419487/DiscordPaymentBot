0c1ae8a364b83c717ace5b65ac2abde6e1ddc525
Jaga <117142388+Jaga7419487@users.noreply.github.com>
2025-01-11 17:12:04 +0800

    small fix on gsheet

--- gsheetTest.py
+++ gsheetTest.py
@@ -1,26 +1,23 @@
+import json
+import os
+
 import pandas as pd
 import pygsheets
 from dotenv import load_dotenv
-import os
-import json
-
 
 """
 Generate the gc file from .env if it doesn't exist
 """
-load_dotenv()
-
-gc_list = ["type", "project_id", "private_key_id", "private_key", "client_email", "client_id", "auth_uri", "token_uri",
-           "auth_provider_x509_cert_url", "client_x509_cert_url", "universe_domain"]
-
-gc_dict = {key: os.getenv(key.upper()) for key in gc_list}
-gc_dict['private_key'] = gc_dict['private_key'].replace('\\n', '\n')
-print(gc_dict)
 if not os.path.exists('discord-payment-bot.json'):
+    gc_list = ["type", "project_id", "private_key_id", "private_key", "client_email", "client_id", "auth_uri",
+               "token_uri",
+               "auth_provider_x509_cert_url", "client_x509_cert_url", "universe_domain"]
+    load_dotenv()
+    gc_dict = {key: os.getenv(key.upper()) for key in gc_list}
+    gc_dict['private_key'] = gc_dict['private_key'].replace('\\n', '\n')
+    print(gc_dict)
     with open('discord-payment-bot.json', 'w') as json_file:
         json.dump(gc_dict, json_file, indent=2)
-
-exit()
 
 """
 https://www.maxlist.xyz/2018/09/25/python_googlesheet_crud/#%E3%84%A7_Python_%E9%80%A3%E7%B5%90_Google_Sheet_API
