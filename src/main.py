980cd592d59bdd7ec5718c9c1347616f5633ffa0
Jaga <117142388+Jaga7419487@users.noreply.github.com>
2025-06-14 15:01:08 +0800

    use decorators on commands

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import asyncio
 
 import discord
 from discord.ext import commands
+from functools import wraps
 
 from constants import BOT_KEY, BOT_STATUS, BOT_DESCRIPTION, SUPPORTED_CURRENCY, TIMEZONE
 from encryption import decrypt_command, encrypt_command
@@ -34,6 +35,26 @@ def start_bot():
     intents = discord.Intents.all()
     bot = commands.Bot(command_prefix='!', intents=intents)
     bot_state = BotState()
+    
+    def require_active_bot(func):
+        """Decorator to require the bot to be active"""
+        @wraps(func)
+        async def wrapper(ctx, *args, **kwargs):
+            if not bot_state.active:
+                await ctx.send("Bot is not started! Call !switch to start the bot")
+                return
+            return await func(ctx, *args, **kwargs)
+        return wrapper
+    
+    def require_payment_channel(func):
+        """Decorator to require the command to be called in the payment channel"""
+        @wraps(func)
+        async def wrapper(ctx, *args, **kwargs):
+            if ctx.channel.id != PAYMENT_CHANNEL_ID:
+                await ctx.send("Please input the record in the **payment** channel")
+                return
+            return await func(ctx, *args, **kwargs)
+        return wrapper
 
     @bot.event
     async def on_ready():
@@ -50,133 +71,104 @@ def start_bot():
                   message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(hidden=True)
+    @require_active_bot
     async def status(message: commands.Context):
-        if bot_state.active:
-            await message.channel.send("Bot is active!")
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await message.channel.send("Bot is active!")
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(help="Show the bot information", brief="Bot information")
+    @require_active_bot
     async def info(message: commands.Context):
-        if bot_state.active:
-            await message.channel.send(BOT_DESCRIPTION)
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await message.channel.send(BOT_DESCRIPTION)
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(name="list", aliases=['l', 'L'], help="List out all payment records stored in the bot",
                  brief="List all payment records")
+    @require_active_bot
     async def show(message: commands.Context):
-        if bot_state.active:
-            await message.channel.send(show_payment_record())
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await message.channel.send(show_payment_record())
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(help=f"Show the latest payment record inputs", brief="Latest payment record inputs")
+    @require_active_bot
     async def log(message: commands.Context):
-        if bot_state.active:
-            response = await message.channel.send('loading...')
-            await response.edit(content=show_payment_logs(message.message.content.lower().split()))
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        response = await message.channel.send('loading...')
+        await response.edit(content=show_payment_logs(message.message.content.lower().split()))
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
             
     @bot.command(help=f"Show all the command inputs",
                  brief="Latest payment record inputs")
+    @require_active_bot
     async def logall(message: commands.Context):
-        if bot_state.active:
-            response = await message.channel.send('loading...')
-            await response.edit(content=show_logs(message.message.content.lower().split()))
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        response = await message.channel.send('loading...')
+        await response.edit(content=show_logs(message.message.content.lower().split()))
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(name='currencies', help="Show all the supported currencies", brief="All supported currencies")
+    @require_active_bot
     async def show_all_currencies(message: commands.Context):
-        if bot_state.active:
-            currency_text = '\n'.join([f"{B(key)}: {value}" for key, value in SUPPORTED_CURRENCY.items()])
-            await message.channel.send(currency_text)
-            write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        currency_text = '\n'.join([f"{B(key)}: {value}" for key, value in SUPPORTED_CURRENCY.items()])
+        await message.channel.send(currency_text)
+        write_log('read', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(help="Create a new user with a name", brief="Create a new user")
+    @require_active_bot
+    @require_payment_channel
     async def create(message: commands.Context):
-        if not bot_state.active:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
-        elif message.channel.id != PAYMENT_CHANNEL_ID:
-            await message.channel.send("Please input the record in the **payment** channel")
-        else:
-            response = await message.channel.send('loading...')
-            result = await create_user(bot, message)
-            await response.edit(content=result)
-            write_log('manage', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-            
+        response = await message.channel.send('loading...')
+        result = await create_user(bot, message)
+        await response.edit(content=result)
+        write_log('manage', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
+
     @bot.command(help="Delete a user if he has no debts", brief="Delete a user")
+    @require_active_bot
+    @require_payment_channel
     async def delete(message: commands.Context):
-        if not bot_state.active:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
-        elif message.channel.id != PAYMENT_CHANNEL_ID:
-            await message.channel.send("Please input the record in the **payment** channel")
-        else:
-            response = await message.channel.send('loading...')
-            result = await delete_user(bot, message)
-            await response.edit(content=result)
-            write_log('manage', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
+        response = await message.channel.send('loading...')
+        result = await delete_user(bot, message)
+        await response.edit(content=result)
+        write_log('manage', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.command(help="Enters a payment record", brief="Enters a payment record")
+    @require_active_bot
+    @require_payment_channel
     async def pm(message: commands.Context):
-        if not bot_state.active:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
-        elif message.channel.id != PAYMENT_CHANNEL_ID:
-            await message.channel.send("Please input the record in the **payment** channel")
-        else:
-            await payment_system(bot, message)
+        await payment_system(bot, message)
             
     @bot.command(aliases=['enc'], help="Encrypt a string with a key", brief="Encrypt a string")
+    @require_active_bot
     async def encrypt(message: commands.Context):
-        if bot_state.active:
-            await encrypt_command(message)  
-            write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await encrypt_command(message)  
+        write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
             
     @bot.command(aliases=['dec'], help="Decrypt a string with a key", brief="Decrypt a string")
+    @require_active_bot
     async def decrypt(message: commands.Context):
-        if bot_state.active:
-            await decrypt_command(message)
-            write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await decrypt_command(message)
+        write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
             
     @bot.command(hidden=True)
+    @require_active_bot
     async def piano(message: commands.Context):
-        if bot_state.active:
-            await piano_system(bot, message)
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        await piano_system(bot, message)
 
     @bot.command(hidden=True)
+    @require_active_bot
     async def emoji(message: commands.Context):
-        if bot_state.active:
-            emoji = str(message.message.content.upper().split()[-1])
-            await message.message.add_reaction(get_emoji(emoji))
-            write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
-                      message.message.created_at.astimezone(TIMEZONE))
-        else:
-            await message.channel.send("Bot is not started! Call !switch to start the bot")
+        emoji = str(message.message.content.upper().split()[-1])
+        await message.message.add_reaction(get_emoji(emoji))
+        write_log('others', channel_to_text(message.channel), message.author.name, message.message.content,
+                    message.message.created_at.astimezone(TIMEZONE))
 
     @bot.event
     async def on_logout():
