dda985f4d6343eaca021d8e915bfd6d78e2122e2
Jaga <117142388+Jaga7419487@users.noreply.github.com>
2026-07-11 18:06:47 +0800

    update !info content

--- src/constants.py
+++ src/constants.py
@@ -6,196 +6,196 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # bot
-INIT_STATE = os.getenv('INIT_STATE', "false").lower() == "true"
-BOT_KEY = os.getenv('BOT_KEY')
-PAYMENT_CHANNEL_ID = int(os.getenv('PAYMENT_CHANNEL_ID'))
-LOG_CHANNEL_ID = int(os.getenv('LOG_CHANNEL_ID'))
+INIT_STATE = os.getenv("INIT_STATE", "false").lower() == "true"
+BOT_KEY = os.getenv("BOT_KEY")
+PAYMENT_CHANNEL_ID = int(os.getenv("PAYMENT_CHANNEL_ID"))
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID"))
 BOT_STATUS = "->> !info"
 UNIFIED_CURRENCY = "HKD"
-VALID_CHARS_SET = set('0123456789+-*/.(（）)')
+VALID_CHARS_SET = set("0123456789+-*/.(（）)")
 SUPPORTED_CURRENCY = {
-  "AED": "United Arab Emirates Dirham",
-  "AFN": "Afghan Afghani",
-  "ALL": "Albanian Lek",
-  "AMD": "Armenian Dram",
-  "ANG": "Netherlands Antillean Guilder",
-  "AOA": "Angolan Kwanza",
-  "ARS": "Argentine Peso",
-  "AUD": "Australian Dollar",
-  "AWG": "Aruban Florin",
-  "AZN": "Azerbaijani Manat",
-  "BAM": "Bosnia-Herzegovina Convertible Mark",
-  "BBD": "Barbadian Dollar",
-  "BDT": "Bangladeshi Taka",
-  "BGN": "Bulgarian Lev",
-  "BHD": "Bahraini Dinar",
-  "BIF": "Burundian Franc",
-  "BMD": "Bermudan Dollar",
-  "BND": "Brunei Dollar",
-  "BOB": "Bolivian Boliviano",
-  "BRL": "Brazilian Real",
-  "BSD": "Bahamian Dollar",
-  "BTC": "Bitcoin",
-  "BTN": "Bhutanese Ngultrum",
-  "BWP": "Botswanan Pula",
-  "BYN": "Belarusian Ruble",
-  "BZD": "Belize Dollar",
-  "CAD": "Canadian Dollar",
-  "CDF": "Congolese Franc",
-  "CHF": "Swiss Franc",
-  "CLF": "Chilean Unit of Account (UF)",
-  "CLP": "Chilean Peso",
-  "CNH": "Chinese Yuan (Offshore)",
-  "CNY": "Chinese Yuan",
-  "COP": "Colombian Peso",
-  "CRC": "Costa Rican Colón",
-  "CUC": "Cuban Convertible Peso",
-  "CUP": "Cuban Peso",
-  "CVE": "Cape Verdean Escudo",
-  "CZK": "Czech Republic Koruna",
-  "DJF": "Djiboutian Franc",
-  "DKK": "Danish Krone",
-  "DOP": "Dominican Peso",
-  "DZD": "Algerian Dinar",
-  "EGP": "Egyptian Pound",
-  "ERN": "Eritrean Nakfa",
-  "ETB": "Ethiopian Birr",
-  "EUR": "Euro",
-  "FJD": "Fijian Dollar",
-  "FKP": "Falkland Islands Pound",
-  "GBP": "British Pound Sterling",
-  "GEL": "Georgian Lari",
-  "GGP": "Guernsey Pound",
-  "GHS": "Ghanaian Cedi",
-  "GIP": "Gibraltar Pound",
-  "GMD": "Gambian Dalasi",
-  "GNF": "Guinean Franc",
-  "GTQ": "Guatemalan Quetzal",
-  "GYD": "Guyanaese Dollar",
-  "HKD": "Hong Kong Dollar",
-  "HNL": "Honduran Lempira",
-  "HRK": "Croatian Kuna",
-  "HTG": "Haitian Gourde",
-  "HUF": "Hungarian Forint",
-  "IDR": "Indonesian Rupiah",
-  "ILS": "Israeli New Sheqel",
-  "IMP": "Manx pound",
-  "INR": "Indian Rupee",
-  "IQD": "Iraqi Dinar",
-  "IRR": "Iranian Rial",
-  "ISK": "Icelandic Króna",
-  "JEP": "Jersey Pound",
-  "JMD": "Jamaican Dollar",
-  "JOD": "Jordanian Dinar",
-  "JPY": "Japanese Yen",
-  "KES": "Kenyan Shilling",
-  "KGS": "Kyrgystani Som",
-  "KHR": "Cambodian Riel",
-  "KMF": "Comorian Franc",
-  "KPW": "North Korean Won",
-  "KRW": "South Korean Won",
-  "KWD": "Kuwaiti Dinar",
-  "KYD": "Cayman Islands Dollar",
-  "KZT": "Kazakhstani Tenge",
-  "LAK": "Laotian Kip",
-  "LBP": "Lebanese Pound",
-  "LKR": "Sri Lankan Rupee",
-  "LRD": "Liberian Dollar",
-  "LSL": "Lesotho Loti",
-  "LYD": "Libyan Dinar",
-  "MAD": "Moroccan Dirham",
-  "MDL": "Moldovan Leu",
-  "MGA": "Malagasy Ariary",
-  "MKD": "Macedonian Denar",
-  "MMK": "Myanma Kyat",
-  "MNT": "Mongolian Tugrik",
-  "MOP": "Macanese Pataca",
-  "MRU": "Mauritanian Ouguiya",
-  "MUR": "Mauritian Rupee",
-  "MVR": "Maldivian Rufiyaa",
-  "MWK": "Malawian Kwacha",
-  "MXN": "Mexican Peso",
-  "MYR": "Malaysian Ringgit",
-  "MZN": "Mozambican Metical",
-  "NAD": "Namibian Dollar",
-  "NGN": "Nigerian Naira",
-  "NIO": "Nicaraguan Córdoba",
-  "NOK": "Norwegian Krone",
-  "NPR": "Nepalese Rupee",
-  "NZD": "New Zealand Dollar",
-  "OMR": "Omani Rial",
-  "PAB": "Panamanian Balboa",
-  "PEN": "Peruvian Nuevo Sol",
-  "PGK": "Papua New Guinean Kina",
-  "PHP": "Philippine Peso",
-  "PKR": "Pakistani Rupee",
-  "PLN": "Polish Zloty",
-  "PYG": "Paraguayan Guarani",
-  "QAR": "Qatari Rial",
-  "RON": "Romanian Leu",
-  "RSD": "Serbian Dinar",
-  "RUB": "Russian Ruble",
-  "RWF": "Rwandan Franc",
-  "SAR": "Saudi Riyal",
-  "SBD": "Solomon Islands Dollar",
-  "SCR": "Seychellois Rupee",
-  "SDG": "Sudanese Pound",
-  "SEK": "Swedish Krona",
-  "SGD": "Singapore Dollar",
-  "SHP": "Saint Helena Pound",
-  "SLL": "Sierra Leonean Leone",
-  "SOS": "Somali Shilling",
-  "SRD": "Surinamese Dollar",
-  "SSP": "South Sudanese Pound",
-  "STD": "São Tomé and Príncipe Dobra (pre-2018)",
-  "STN": "São Tomé and Príncipe Dobra",
-  "SVC": "Salvadoran Colón",
-  "SYP": "Syrian Pound",
-  "SZL": "Swazi Lilangeni",
-  "THB": "Thai Baht",
-  "TJS": "Tajikistani Somoni",
-  "TMT": "Turkmenistani Manat",
-  "TND": "Tunisian Dinar",
-  "TOP": "Tongan Pa'anga",
-  "TRY": "Turkish Lira",
-  "TTD": "Trinidad and Tobago Dollar",
-  "TWD": "New Taiwan Dollar",
-  "TZS": "Tanzanian Shilling",
-  "UAH": "Ukrainian Hryvnia",
-  "UGX": "Ugandan Shilling",
-  "USD": "United States Dollar",
-  "UYU": "Uruguayan Peso",
-  "UZS": "Uzbekistan Som",
-  "VEF": "Venezuelan Bolívar Fuerte (Old)",
-  "VES": "Venezuelan Bolívar Soberano",
-  "VND": "Vietnamese Dong",
-  "VUV": "Vanuatu Vatu",
-  "WST": "Samoan Tala",
-  "XAF": "CFA Franc BEAC",
-  "XAG": "Silver Ounce",
-  "XAU": "Gold Ounce",
-  "XCD": "East Caribbean Dollar",
-  "XDR": "Special Drawing Rights",
-  "XOF": "CFA Franc BCEAO",
-  "XPD": "Palladium Ounce",
-  "XPF": "CFP Franc",
-  "XPT": "Platinum Ounce",
-  "YER": "Yemeni Rial",
-  "ZAR": "South African Rand",
-  "ZMW": "Zambian Kwacha",
-  "ZWL": "Zimbabwean Dollar"
+    "AED": "United Arab Emirates Dirham",
+    "AFN": "Afghan Afghani",
+    "ALL": "Albanian Lek",
+    "AMD": "Armenian Dram",
+    "ANG": "Netherlands Antillean Guilder",
+    "AOA": "Angolan Kwanza",
+    "ARS": "Argentine Peso",
+    "AUD": "Australian Dollar",
+    "AWG": "Aruban Florin",
+    "AZN": "Azerbaijani Manat",
+    "BAM": "Bosnia-Herzegovina Convertible Mark",
+    "BBD": "Barbadian Dollar",
+    "BDT": "Bangladeshi Taka",
+    "BGN": "Bulgarian Lev",
+    "BHD": "Bahraini Dinar",
+    "BIF": "Burundian Franc",
+    "BMD": "Bermudan Dollar",
+    "BND": "Brunei Dollar",
+    "BOB": "Bolivian Boliviano",
+    "BRL": "Brazilian Real",
+    "BSD": "Bahamian Dollar",
+    "BTC": "Bitcoin",
+    "BTN": "Bhutanese Ngultrum",
+    "BWP": "Botswanan Pula",
+    "BYN": "Belarusian Ruble",
+    "BZD": "Belize Dollar",
+    "CAD": "Canadian Dollar",
+    "CDF": "Congolese Franc",
+    "CHF": "Swiss Franc",
+    "CLF": "Chilean Unit of Account (UF)",
+    "CLP": "Chilean Peso",
+    "CNH": "Chinese Yuan (Offshore)",
+    "CNY": "Chinese Yuan",
+    "COP": "Colombian Peso",
+    "CRC": "Costa Rican Colón",
+    "CUC": "Cuban Convertible Peso",
+    "CUP": "Cuban Peso",
+    "CVE": "Cape Verdean Escudo",
+    "CZK": "Czech Republic Koruna",
+    "DJF": "Djiboutian Franc",
+    "DKK": "Danish Krone",
+    "DOP": "Dominican Peso",
+    "DZD": "Algerian Dinar",
+    "EGP": "Egyptian Pound",
+    "ERN": "Eritrean Nakfa",
+    "ETB": "Ethiopian Birr",
+    "EUR": "Euro",
+    "FJD": "Fijian Dollar",
+    "FKP": "Falkland Islands Pound",
+    "GBP": "British Pound Sterling",
+    "GEL": "Georgian Lari",
+    "GGP": "Guernsey Pound",
+    "GHS": "Ghanaian Cedi",
+    "GIP": "Gibraltar Pound",
+    "GMD": "Gambian Dalasi",
+    "GNF": "Guinean Franc",
+    "GTQ": "Guatemalan Quetzal",
+    "GYD": "Guyanaese Dollar",
+    "HKD": "Hong Kong Dollar",
+    "HNL": "Honduran Lempira",
+    "HRK": "Croatian Kuna",
+    "HTG": "Haitian Gourde",
+    "HUF": "Hungarian Forint",
+    "IDR": "Indonesian Rupiah",
+    "ILS": "Israeli New Sheqel",
+    "IMP": "Manx pound",
+    "INR": "Indian Rupee",
+    "IQD": "Iraqi Dinar",
+    "IRR": "Iranian Rial",
+    "ISK": "Icelandic Króna",
+    "JEP": "Jersey Pound",
+    "JMD": "Jamaican Dollar",
+    "JOD": "Jordanian Dinar",
+    "JPY": "Japanese Yen",
+    "KES": "Kenyan Shilling",
+    "KGS": "Kyrgystani Som",
+    "KHR": "Cambodian Riel",
+    "KMF": "Comorian Franc",
+    "KPW": "North Korean Won",
+    "KRW": "South Korean Won",
+    "KWD": "Kuwaiti Dinar",
+    "KYD": "Cayman Islands Dollar",
+    "KZT": "Kazakhstani Tenge",
+    "LAK": "Laotian Kip",
+    "LBP": "Lebanese Pound",
+    "LKR": "Sri Lankan Rupee",
+    "LRD": "Liberian Dollar",
+    "LSL": "Lesotho Loti",
+    "LYD": "Libyan Dinar",
+    "MAD": "Moroccan Dirham",
+    "MDL": "Moldovan Leu",
+    "MGA": "Malagasy Ariary",
+    "MKD": "Macedonian Denar",
+    "MMK": "Myanma Kyat",
+    "MNT": "Mongolian Tugrik",
+    "MOP": "Macanese Pataca",
+    "MRU": "Mauritanian Ouguiya",
+    "MUR": "Mauritian Rupee",
+    "MVR": "Maldivian Rufiyaa",
+    "MWK": "Malawian Kwacha",
+    "MXN": "Mexican Peso",
+    "MYR": "Malaysian Ringgit",
+    "MZN": "Mozambican Metical",
+    "NAD": "Namibian Dollar",
+    "NGN": "Nigerian Naira",
+    "NIO": "Nicaraguan Córdoba",
+    "NOK": "Norwegian Krone",
+    "NPR": "Nepalese Rupee",
+    "NZD": "New Zealand Dollar",
+    "OMR": "Omani Rial",
+    "PAB": "Panamanian Balboa",
+    "PEN": "Peruvian Nuevo Sol",
+    "PGK": "Papua New Guinean Kina",
+    "PHP": "Philippine Peso",
+    "PKR": "Pakistani Rupee",
+    "PLN": "Polish Zloty",
+    "PYG": "Paraguayan Guarani",
+    "QAR": "Qatari Rial",
+    "RON": "Romanian Leu",
+    "RSD": "Serbian Dinar",
+    "RUB": "Russian Ruble",
+    "RWF": "Rwandan Franc",
+    "SAR": "Saudi Riyal",
+    "SBD": "Solomon Islands Dollar",
+    "SCR": "Seychellois Rupee",
+    "SDG": "Sudanese Pound",
+    "SEK": "Swedish Krona",
+    "SGD": "Singapore Dollar",
+    "SHP": "Saint Helena Pound",
+    "SLL": "Sierra Leonean Leone",
+    "SOS": "Somali Shilling",
+    "SRD": "Surinamese Dollar",
+    "SSP": "South Sudanese Pound",
+    "STD": "São Tomé and Príncipe Dobra (pre-2018)",
+    "STN": "São Tomé and Príncipe Dobra",
+    "SVC": "Salvadoran Colón",
+    "SYP": "Syrian Pound",
+    "SZL": "Swazi Lilangeni",
+    "THB": "Thai Baht",
+    "TJS": "Tajikistani Somoni",
+    "TMT": "Turkmenistani Manat",
+    "TND": "Tunisian Dinar",
+    "TOP": "Tongan Pa'anga",
+    "TRY": "Turkish Lira",
+    "TTD": "Trinidad and Tobago Dollar",
+    "TWD": "New Taiwan Dollar",
+    "TZS": "Tanzanian Shilling",
+    "UAH": "Ukrainian Hryvnia",
+    "UGX": "Ugandan Shilling",
+    "USD": "United States Dollar",
+    "UYU": "Uruguayan Peso",
+    "UZS": "Uzbekistan Som",
+    "VEF": "Venezuelan Bolívar Fuerte (Old)",
+    "VES": "Venezuelan Bolívar Soberano",
+    "VND": "Vietnamese Dong",
+    "VUV": "Vanuatu Vatu",
+    "WST": "Samoan Tala",
+    "XAF": "CFA Franc BEAC",
+    "XAG": "Silver Ounce",
+    "XAU": "Gold Ounce",
+    "XCD": "East Caribbean Dollar",
+    "XDR": "Special Drawing Rights",
+    "XOF": "CFA Franc BCEAO",
+    "XPD": "Palladium Ounce",
+    "XPF": "CFP Franc",
+    "XPT": "Platinum Ounce",
+    "YER": "Yemeni Rial",
+    "ZAR": "South African Rand",
+    "ZMW": "Zambian Kwacha",
+    "ZWL": "Zimbabwean Dollar",
 }
 USER_MAPPING = {  # for mentioning
-    'jaga': '635760265975169044',
-    'larry': '641546398764105759',
-    '741': '688994217745580067',
-    'sdr': '616617935204777985',
-    'tom': '707067744851329077',
-    'hjl': '410441332012613634',
-    'ryan': '619092133936365570',
-    '6uo': '597352233490841601',
-    'inevitable': '695475297675640882',
-    'andes': '621959791698247680',
+    "jaga": "635760265975169044",
+    "larry": "641546398764105759",
+    "741": "688994217745580067",
+    "sdr": "616617935204777985",
+    "tom": "707067744851329077",
+    "hjl": "410441332012613634",
+    "ryan": "619092133936365570",
+    "6uo": "597352233490841601",
+    "inevitable": "695475297675640882",
+    "andes": "621959791698247680",
 }
 
 # digits
@@ -207,27 +207,29 @@ UNDO_TIMEOUT = 3600.0
 ENCRYPTED_DELETE_TIMEOUT = 15
 
 # koyeb
-KOYEB_PUBLIC_LINK = os.getenv('KOYEB_PUBLIC_LINK')
-TIMEZONE = pytz.timezone('Asia/Hong_Kong')  # ensure consistency between firestore and discord
+KOYEB_PUBLIC_LINK = os.getenv("KOYEB_PUBLIC_LINK")
+TIMEZONE = pytz.timezone(
+    "Asia/Hong_Kong"
+)  # ensure consistency between firestore and discord
 
 # firebase
 FIREBASE_KEY_PATH = "discord-payment-bot-firebase-adminsdk.json"
 FIREBASE_KEY = {
-	"type": "service_account",
-	"project_id": os.getenv('PROJECT_ID'),
-  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
-  "private_key": os.getenv('PRIVATE_KEY').replace('\\n', '\n'),
-  "client_email": os.getenv('CLIENT_EMAIL'),
-  "client_id": os.getenv('CLIENT_ID'),
-	"auth_uri": "https://accounts.google.com/o/oauth2/auth",
-	"token_uri": "https://oauth2.googleapis.com/token",
-	"auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": os.getenv('CLIENT_X509_CERT_URL'),
-	"universe_domain": "googleapis.com"
+    "type": "service_account",
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": "googleapis.com",
 }
 
 # open exchange rate (for exchange rates)
-OPEN_EXCHANGE_RATE_API_KEY = os.getenv('OPEN_EXCHANGE_RATE_API_KEY')
+OPEN_EXCHANGE_RATE_API_KEY = os.getenv("OPEN_EXCHANGE_RATE_API_KEY")
 
 # emoji
 EMOJI_MAPPING = {
@@ -271,43 +273,42 @@ EMOJI_MAPPING = {
     "-": "➖",
     "*": "✖️",
     "/": "➗",
-    "?": '❓',
+    "?": "❓",
     "TICK": "✅",
     "CROSS": "❌",
 }
 
+# Beware of 2000 character max length limit
 BOT_DESCRIPTION = """
 # Discord Payment Bot
-## Purpose: Manage and record payments among a group
-**Workflow:**
-1. **Scenario:** When __Person A__ pays for __Person B__, __Person B__ owes __Person A__
-2. **Centralized System:** A central person manages transactions, ensuring smooth exchanges
-3. **Repayment Chain:** 
-  - __Person A__ pays for __Person B__ → __Person B__ owes __Person A__
-  - __Person B__ owes __Centralized Person__ → __Centralized Person__ owes __Person A__
-**Bot Functionality:**
-- **Interaction:** Users call the bot with '!' prefix (e.g., `!info`)
-- **Response:** The bot responds to valid commands with appropriate messages
-## Commands
-`!info`: Display this information
-`!list`: List all payment records, short: `!l`
-`!create [name]`: Create a new user (e.g., `!create personA`)
-`!delete [name]`: Delete a user with no debts (e.g., `!delete personA`)
-`!history`: Show the history of command inputs
-`!currencies`: Show supported currencies
-`!encrypt`: Encrypt a message with a secret key
-`!decrypt`: Decrypt an encrypted message with a key
-**`!pm`: Enter a payment record (UI window if only `!pm` sent)**
-> __Syntax:__
-> `!pm [payee] [operation] [get paid] [amount] [-cur] [sc] [reason]`
-> `payee`: People who should repay, separated by ',' without spaces
-> `operation`: `owe`/`payback`
-> `get paid`: Person to be repaid
-> `amount`: Up to 3 decimal points
-> `-cur`: Optional: `HKD/CNY/GBP` (default `HKD`)
-> `sc`: Optional: include 10% service charge
-> `reason`: Optional, no brackets needed
-> Example: `!pm personA,personB owe personC 100 -CNY sc example reason`
 
-- Developed by __Jaga Chau__ 25-12-2023
+## Purpose: Manage and record payments among a group
+
+## Commands
+
+### General
+- `!info`: Show this help text.
+- `!status`: Check whether the bot is currently active.
+- `!switch`: Toggle the bot on or off.
+- `!list`, `!l`: Show the current payment summary for all users.
+- `!history [command_type] [number]`: Show recent logs. `command_type` can be `payment`/`manage`/`read`/`others`/`all`. `number` defaults to 10.
+- `!currencies`: List all supported currency codes.
+### Payment management
+- `!create <name>`: Create a new user in the payment system. Use this in the payment channel.
+- `!delete <name>`: Delete a user with no outstanding balance. Use this in the payment channel.
+- `!pm`: Open the payment UI to enter a record manually.
+- `!pm <payees> <owe|payback> <receiver> <amount> [-CUR] [sc] [reason]`: Record a payment directly.
+  - `<payees>`: One or more users separated by commas, with no spaces.
+  - `<owe|payback>`: Choose whether the listed users owe or pay back.
+  - `<receiver>`: The user being repaid.
+  - `<amount>`: Numeric value, up to 3 decimal places.
+  - `[-CUR]`: Optional currency code such as `-HKD`, `-CNY`, or `-GBP`. Default is `HKD`.
+  - `sc`: Add a 10% service charge.
+  - `[reason]`: Optional free-form note.
+  - Example: `!pm pplA,pplB owe pplC 10 -CNY sc example reason`
+### Utilities
+- `!encrypt`: Open the encryption modal to encrypt a message with a key.
+- `!decrypt`: Open the decryption modal to decrypt a message with a key.
+
+Developed by __Jaga Chau__ 25-12-2023
 """
